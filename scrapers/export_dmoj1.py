208b98448c23dc54069da5b34355e0a20c70df67
NigusFs <nicolas.floress@mail.udp.cl>
2020-10-12 22:53:40 -0300

    Final extract one problem of DMO

--- scrapers/export_dmoj1.py
+++ scrapers/export_dmoj1.py
@@ -1,19 +1,61 @@
 import requests
 from bs4 import BeautifulSoup as bs1
 import re
+from dataclasses import dataclass
+from typing import List
 
-url = 'https://dmoj.ca/problem/16bitswonly'
-page = requests.get(url)
+@dataclass
+class TestCase:
+	test_input: str
+	output : str
 
-soup = bs1(page.content, 'html.parser')
+@dataclass
+class Problem:
+	title: str
+	body: str
+	dificulty: str
+	source: str
+	testcase: List[TestCase]
+	category: List[str]
 
-titulo = soup.find("h2",{"style": "color:#393630; display: inline-block"}).text
+def extract_problem(url):
+	#url = 'https://dmoj.ca/problem/boardgames'
+	page = requests.get(url)
 
-contenido_raw = soup.find("div",{"class": "content-description screen"}).text
+	soup = bs1(page.content, 'html.parser')
 
-#print(contenido_raw)
-enunciado =  re.search(r'(.*)Sample Input',contenido_raw,re.DOTALL)[1].replace('\n', '\n\n ')
-input_sample_case =  re.search(r'Sample Input\n(.*)Sample Output',contenido_raw,re.DOTALL)[1]
-output_sample_case =  re.search(r'Sample Output\n(.*)',contenido_raw,re.DOTALL)[1]
+	titulo = soup.find("h2",{"style": "color:#393630; display: inline-block"}).text
 
-print(enunciado)
+	contenido_raw = soup.find("div",{"class": "content-description screen"}).text
+
+	categoria =soup.find("div",{"class": "toggled"}).text.split(", ")
+
+	test_case= []
+	try:
+		many_test_cases =False
+
+		input_sample_case =  re.search(r'Sample Input\n(.*)Sample Output',contenido_raw,re.DOTALL)[1]
+		output_sample_case =  re.search(r'Sample Output\n(.*)',contenido_raw,re.DOTALL)[1]
+		test_case.append(TestCase(input_sample_case,output_sample_case))
+
+	except:
+		many_test_cases = True
+		input_sample_case =  re.findall(r'Sample Input \d(.+?)Sample Output',contenido_raw,re.DOTALL)
+		output_sample_case = re.findall(r'Sample Output \d(.+?)\n',contenido_raw,re.DOTALL)
+
+		for i in range(len(input_sample_case)):
+			t1=TestCase(input_sample_case[i],output_sample_case[i])
+			test_case.append(t1)
+		
+
+	if many_test_cases:
+		enunciado =  re.search(r'(.*)Sample Input 1',contenido_raw,re.DOTALL)[1].replace('\n', '\n\n ') #sacar el replace si no se quiere con tantos espacios
+	else:
+		enunciado =  re.search(r'(.*)Sample Input',contenido_raw,re.DOTALL)[1].replace('\n', '\n\n ') #sacar el replace si no se quiere con tantos espacios
+	
+
+	dificultad=" "
+
+	return Problem(titulo,enunciado,dificultad,"DMOJ",test_case,categoria)
+	
+
