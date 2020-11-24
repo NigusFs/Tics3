c8f1add858c8b378e38def399c4dff96e3a42e21
NigusFs <nicolas.floress@mail.udp.cl>
2020-11-24 20:47:52 -0300

    fixing scraper dmoj, now capture correctly testcases

--- problemFinder/finder/scrapers/dmoj.py
+++ problemFinder/finder/scrapers/dmoj.py
@@ -23,7 +23,7 @@ class Problem:
 def extract_many(page=1) -> List[Problem] :
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 	response = requests.get("https://dmoj.ca/problems/?page={}".format(page),headers=headers)
-	print(response)
+	response.close()
 	soup = bs1(response.content, 'html.parser')
 	table_problems = str(soup.find("table"))
 	list_code_problems=re.findall(r'\/problem\/([a-z A-Z 0-9]+)\"',table_problems)
@@ -33,10 +33,12 @@ def extract_many(page=1) -> List[Problem] :
 def extract_problem(code) -> Problem:
 	url = 'https://dmoj.ca/problem/{}'.format(code)
 	page = requests.get(url)
+	page.close()
 	soup = bs1(page.content, 'html.parser')
 
 	titulo = soup.find("h2",{"style": "color:#393630; display: inline-block"}).text
 	contenido_raw = soup.find("div",{"class": "content-description screen"}).text
+
 	categoria =soup.find("div",{"class": "toggled"}).text.split(", ")
 	test_case, many_test_cases=extract_testcase(contenido_raw)
 	enunciado=extract_enunciado(contenido_raw,many_test_cases)
@@ -44,8 +46,8 @@ def extract_problem(code) -> Problem:
 	
 	time.sleep(1.5)
 	#print(Problem(titulo,enunciado,dificultad,"DMOJ",test_case,categoria))
-	# print("titulo: ", titulo)
-	# print("content: " ,enunciado)
+	#print("titulo: ", titulo)
+	#print("content: " ,contenido_raw)
 	# print("dificultad: ", dificultad)
 	# print("source: ","DMOJ")
 	# for i in test_case:
@@ -59,24 +61,24 @@ def extract_problem(code) -> Problem:
 def extract_testcase(contenido_raw):
 	test_case= []
 
-	if  re.search(r'Sample Input\n(.*)Sample Output',contenido_raw,re.DOTALL) != None:
+	if  re.search(r'Sample Input\n(.+?)Sample Output',contenido_raw,re.DOTALL) != None:
 		many_test_cases =False
-		input_sample_case =  re.search(r'Sample Input\n(.*)Sample Output',contenido_raw,re.DOTALL)[1].strip()
-		output_sample_case =  re.search(r'Sample Output\n(.*)',contenido_raw,re.DOTALL)[1].strip()
+		input_sample_case =  re.search(r'Sample Input\n(.+?)Sample Output',contenido_raw,re.DOTALL)[1].strip()
+		output_sample_case =  re.search(r'\nSample Output\n(.+?)\n(Explanation|\n)',contenido_raw,re.DOTALL)[1].strip()
 		test_case.append(TestCase(input_sample_case,output_sample_case))
 
 	elif re.findall(r'Sample Input \d(.+?)Sample Output',contenido_raw,re.DOTALL) != None :
 		many_test_cases = True
 		input_sample_case =  re.findall(r'Sample Input \d(.+?)Sample Output',contenido_raw,re.DOTALL)
-		output_sample_case = re.findall(r'Sample Output \d(.+?)\n',contenido_raw,re.DOTALL)
-
+		output_sample_case = re.findall(r'\nSample Output \d(.+?)\n(Sample Input \d|\n|Explanation|Sample Explanation )',contenido_raw,re.DOTALL)
+		
 		for i in range(len(input_sample_case)):
-			t1=TestCase(input_sample_case[i].strip(),output_sample_case[i].strip())
+			t1=TestCase(input_sample_case[i].strip(),output_sample_case[i][0])
 			test_case.append(t1)
 	else:
 		many_test_cases = None
 		test_case.append(TestCase(None,None))
-		print("No testcases")
+		#print("No testcases")
 
 	return test_case,many_test_cases 
 
@@ -92,11 +94,13 @@ def extract_enunciado(contenido_raw,many_test_cases ):
 	return enunciado
 
 def extract_dificultad(code):
-	points_problem = requests.get("https://dmoj.ca/api/v2/problem/{}".format(code)).json()['data']['object']['points']
-
-	if  points_problem >40:
+	response = requests.get("https://dmoj.ca/api/v2/problem/{}".format(code))
+	points_problem = response.json()['data']['object']['points']
+	print(points_problem)
+	response.close()
+	if  points_problem >=30:
 		dificultad="Dificil"
-	elif points_problem >20:
+	elif points_problem >=10:
 		dificultad="Medio"
 	else:
 		dificultad="Facil"
